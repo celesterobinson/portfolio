509637f8ad47984fbcab279ddd3831677a184bb2
Celeste Robinson <celestekrobinson@gmail.com>
2018-02-23 15:58:15 -0700

    added rel to a tags in footer

--- src/App/Footer/index.js
+++ src/App/Footer/index.js
@@ -9,13 +9,13 @@ import "../styles/Footer.css";
 function Footer() {
     return (
         <div className="footer">
-            <a href="https://twitter.com/cececodes" target="_blank">
+            <a href="https://twitter.com/cececodes" rel="noopener noreferrer" target="_blank">
                 <img src={Twitter} alt="Twitter Link" />
             </a>
-            <a href="https://github.com/celesterobinson" target="_blank">
+            <a href="https://github.com/celesterobinson" rel="noopener noreferrer" target="_blank">
                 <img src={Github} alt="Github Link" />
             </a>
-            <a href="https://www.linkedin.com/in/celeste-robinson/" target="_blank">
+            <a href="https://www.linkedin.com/in/celeste-robinson/" rel="noopener noreferrer" target="_blank">
                 <img src={Linkedin} alt="LinkedIn Link" />
             </a>
         </div>
