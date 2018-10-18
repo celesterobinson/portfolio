22232b6709ed3daaace8be514f54e2a3cda8f481
Celeste Robinson <crobinso@adobe.com>
2018-10-18 15:35:39 -0600

    removed launch script

---
+++ cypress/integration/examples/sample_spec.js
@@ -0,0 +1,12 @@
+describe('My First Test', function () {
+    it('Visits the kitchen sink', function () {
+        // Arrange - setup initial app state
+        // - visit a web page
+        cy.visit('https://example.cypress.io')
+        // - query for an element
+        // Act - take an action
+        // - interact with that element
+        // Assert - make an assertion
+        // - make an assertion about page content
+    })
+})
