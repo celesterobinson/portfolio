6ff0f02bb9e5fffac326cd150bc11f9f02eaded6
Celeste Robinson <crobinso@adobe.com>
2018-10-30 13:54:36 -0600

    adjusted navbar text color

--- src/App/routes/Homepage/index.js
+++ src/App/routes/Homepage/index.js
@@ -8,7 +8,7 @@ export default class Homepage extends Component {
     render() {
         return (
             <div>
-                <Navbar style={{color: "white"}}/>
+                <Navbar style={{color: "white!important"}}/>
                 <div className="homepage" id="home">
                     <h2>Hey. Thanks for visiting. I'm</h2>
                     <h1 className="name">C&eacute;leste Robinson</h1>
