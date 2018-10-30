8412eede346299d030f110dc06f81fb9f531cc67
Celeste Robinson <crobinso@adobe.com>
2018-10-30 13:43:46 -0600

    Added navbar to homepage

--- src/App/routes/Homepage/index.js
+++ src/App/routes/Homepage/index.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import "../../styles/Homepage.css";
+import Navbar from "../../Navbar/index.js";
+
 
 export default class Homepage extends Component {
 
     render() {
         return (
-            <div className="homepage" id="home">
-                <h2>Hey. Thanks for visiting. I'm</h2>
-                <h1 className="name">C&eacute;leste Robinson</h1>
-                <h2>Web developer, gamer, and Harry Potter addict.</h2>
+            <div>
+                <Navbar />
+                <div className="homepage" id="home">
+                    <h2>Hey. Thanks for visiting. I'm</h2>
+                    <h1 className="name">C&eacute;leste Robinson</h1>
+                    <h2>Web developer, gamer, and Harry Potter addict.</h2>
+                </div>
             </div>
         )
     }
